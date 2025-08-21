44923e92d1572fa1e1e57a6ba50ef595cf4b754a
liuyi <liuyiinhhu@gmail.com>
2025-08-21 20:47:22 +0800

    feat(search): 添加全文搜索功能并优化开发流程
    - 在 docusaurus.config.ts 中添加了 docusaurus-lunr-search 插件，实现全文搜索功能
    - 在 package.json 中增加了 start-server 脚本，便于本地预览和开发
    - 更新了格式化代码的脚本，确保代码风格一致性

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -27,6 +28,7 @@ const config: Config = {
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
+    plugins: [require.resolve('docusaurus-lunr-search')],
 
     // Even if you don't use internationalization, you can use this field to set
     // useful metadata like html lang. For example, if your site is Chinese, you
